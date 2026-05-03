2167ea8040129a57042e2bafa72d8c1ee63ad351
Maximo Benjamin Rodriguez <maximobenjaminrodriguez01@gmail.com>
2026-05-03 20:42:01 -0300

    Problema 6 resuelto: Se implemento metodo de extension para strings

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        public decimal ImporteBase { get; set; }
+
+        public Sale(decimal importeBase)
+        {
+            ImporteBase = importeBase;
+        }
+        public virtual decimal CalculateTotal()
+        {
+            return ImporteBase;
+        }
+    }
+
+    public class RetailSale : Sale
+    {
+        public RetailSale(decimal importeBase) : base(importeBase) { }
+
+        public override decimal CalculateTotal()
+        {
+            return ImporteBase;
+        }
+    }
+
+    public class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal importeBase) : base(importeBase) { }
+
+        public override decimal CalculateTotal()
+        {
+            return ImporteBase * 0.90m;
+        }
+    }
+
+    public class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+}
